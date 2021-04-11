7804b250ac3ed2f5965b5f1c70aacfbe16b980b6
draculidze <draculidze@mail.ru>
2021-04-11 21:38:29 +0300

    Добавлен сайт

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+    $("#app").vide({
+        mp4: "video/mk_bg.mp4",
+        webm: "video/mk_bg.webm",
+        ogv: "video/mk_bg.ogv",
+        poster: "img/bg.jpg",
+    }, {
+        resizing: true,
+        bgColor: 'transparent',
+        position: 'center right'
+    });
+});*/
